cb7d071dc7429d5ab025145b5b3ee7d52de29f72
Rafael <rafakawagoe@gmail.com>
2025-11-30 01:40:29 -0300

    chore(backend): upgrade pydantic settings config to v2

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,9 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     OPEN_METEO_GEOCODING_URL: str = "https://geocoding-api.open-meteo.com/v1/search"
     OPEN_METEO_WEATHER_URL: str = "https://api.open-meteo.com/v1/forecast"
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
